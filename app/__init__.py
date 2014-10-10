78e21bf4445a9f1762af237926fc0344cc6f447d
teddymcw <teddymcw@gmail.com>
2014-10-10 16:17:59 -0700

    templates up need to rediect accordingly

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask#, render_template, flash
+from flask import Flask, render_template, flash
 from flask.ext.sqlalchemy import SQLAlchemy
+from forms import InputForm
 
 #for config
 import os
@@ -23,6 +24,24 @@ app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 @app.route("/")
 def hello():
     return "test success!!!"
+
+def print_board():   
+    return board[:3], board[3:6], board[6:9]
+
+#test application code for db, forms and such
+board = [str(num) for num in range(10)]
+@app.route("/play", methods=["GET", "POST"])
+def show_board():
+    new_board = print_board()
+    form=InputForm()
+    if form.validate_on_submit():
+        flash("you are validated")
+    else:
+        flash("ehhh...ehh!, form didn't go")
+    return render_template("test.html", form=form, board_1=new_board[0], \
+                                                    board_2=new_board[1], \
+                                                    board_3=new_board[2])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ app/forms.py
@@ -0,0 +1,7 @@
+from flask.ext.wtf import Form 
+from wtforms import TextField
+from wtforms.validators import DataRequired, Length
+
+class InputForm(Form):
+    x_or_o = TextField(u'x-or-o', validators=[DataRequired(), \
+        Length(min=1, max=1, message="input must be 'x' or 'o'")])
