52c463d3cd06b90fdb348f0d65900887d41fef47
teddymcw <teddymcw@gmail.com>
2014-10-13 23:08:10 -0700

    added extra planning routes and stuff i think

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request
 from flask.ext.sqlalchemy import SQLAlchemy
 from forms import BasicForm, InputForm
 
@@ -11,7 +11,7 @@ db = SQLAlchemy(app)
 
 #make shift config section
 #    below is for CSFR prevention by wtforms
-app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SECRET_KEY'] = 'something'
 #find this file
 basedir = os.path.abspath(os.path.dirname(__file__))
 #db section
@@ -25,19 +25,23 @@ app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 def hello():
     return "test success!!!"
 
-def print_board():   
-    return board[:3], board[3:6], board[6:9]
 
 @app.route("/basic_form", methods=["GET", "POST"])
 def basic_form():
     form = BasicForm()
-    if form.validate_on_submit():
+    if form:
         flash("you are validated")
+        data = form.basic_form.data
+        print(data)
         print 'valid test'
     else:
         flash("ehhh...ehh!, form didn't go")
         print 'invalid test'
     return render_template("basic_form.html", form=form)
+
+#util function for ttt
+def print_board():   
+    return board[:3], board[3:6], board[6:9]
 
 #test application code for db, forms and such
 board = [str(num) for num in range(10)]
@@ -55,6 +59,14 @@ def show_board():
     return render_template("test.html", form=form, board_1=new_board[0], \
                                                     board_2=new_board[1], \
                                                     board_3=new_board[2])
+  
+#route to display some of flasks features
+@app.route("/fun")
+def fun():
+    user_agent = request.headers.get('User-Agent')
+    print(dir(request.headers))
+    return '<p>Your browser is %s</p>' % user_agent
+
 
 
 if __name__ == "__main__":
--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField #TextField,
 from wtforms.validators import DataRequired, Length
 
 class BasicForm(Form):
-    basic_form = StringField("the_basic_form", validators=[DataRequired()])
+    basic_form = StringField("the_basic_form")#, validators=[DataRequired()])
 
 class InputForm(Form):
     x_or_o = StringField(u'x-or-o', validators=[DataRequired(), \
